9f463e43a9217030ec3ca30d95336453943e0197
SaravanaBhavan <SaravanaBhavan@SaravanaBhavan>
2020-02-26 23:11:09 +0530

    Project Creation

---
+++ submitDocument/src/main/java/com/sarav/submitdocument/SubmitDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.sarav.submitdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmitDocumentApplication.class, args);
+	}
+
+}
